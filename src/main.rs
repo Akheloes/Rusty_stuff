607ae9339bb7dcd8b0c01e31332bcd045413cec0
Faical OUDBIB <faical.oudbib@ext.euronews.com>
2023-04-15 19:42:58 +0200

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,19 @@
-use std::f32;
-
-fn main() {
-    let top_point: Point = Point { x: 2.0, y: 3.0 };
-    let bottom_point: Point = Point { x: 1.0, y: 2.0 };
-
-    println!("{}", rectangle_area(top_point, bottom_point));
-}
-
 #[derive(Debug)]
-struct Point {
-    x: f32,
-    y: f32,
+enum Command {
+    Query,
+    Mutate,
+    BiSequence { first: String, second: String },
 }
 
-fn rectangle_area(top: Point, bottom: Point) -> f32 {
-    let Point { x: top_x, y: top_y } = top;
-    let Point { x: bottom_x, y: bottom_y } = bottom;
-
-    return axial_distance(top_x, bottom_x) * axial_distance(top_y, bottom_y);
+fn handle(command: Command) {
+    match command {
+        Command::Query => println!("Execute query"),
+        Command::Mutate => println!("Execute mutation"),
+        Command::BiSequence { first, second } => println!("{} -> {}", first, second),
+    }
 }
-
-fn axial_distance(top_x: f32, bottom_x: f32) -> f32 {
-    f32::abs(top_x - bottom_x)
+fn main() {
+    handle(Command::Query);
+    handle(Command::Mutate);
+    handle(Command::BiSequence { first: String::from("First command"), second: String::from("Second command") });
 }
