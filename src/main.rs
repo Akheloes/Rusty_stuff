578734b936c6200256175f16e9face1e27ae9026
Faical OUDBIB <faical.oudbib@ext.euronews.com>
2023-04-22 10:41:20 +0200

    use scoping

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,14 @@ enum Command {
     BiSequence { first: String, second: String },
 }
 
-impl Command {
-    fn execute(&self, code: i8) {
-        match self {
-            Self::Query => println!("Query {}", code),
-            Self::Mutate => println!("Mutation {}", code),
-            Self::BiSequence { first, second } => println!("BiSequence {}", code),
-        }
-    }
-}
-
-fn handle(command: Command) {
-    match command {
-        Command::Query => println!("Execute query"),
-        Command::Mutate => println!("Execute mutation"),
-        Command::BiSequence { first, second } => println!("{} -> {}", first, second),
-    }
-}
 fn main() {
-    handle(Command::Query);
-    handle(Command::Mutate);
-    let bi_seq_command = Command::BiSequence { first: String::from("First command"), second: String::from("Second command") };
-    handle(bi_seq_command);
+    use crate::Command::*;
 
-    Command::execute(&Command::Query, 10);
+    let called = Query;
+
+    match called {
+        Query => println!("Called Query"),
+        Mutate => println!("Called Mutate"),
+        BiSequence { first, second } => println!("Called BiSequence {{ first: {}, second: {}}}", first, second),
+    }
 }
