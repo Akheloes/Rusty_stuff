bf5a7010e2f8638edfeed4f950fb37584ce96f30
Faical OUDBIB <faical.oudbib@ext.euronews.com>
2023-04-22 11:07:46 +0200

    c-like-enums

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
-// Code some rust and execute 'cargo run'
-fn main() {
+#![allow(dead_code)]
 
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+enum Color {
+    Red = 0xff0000,
+}
+
+fn main() {
+    let number_zero = Number::Zero as i8;
+    let number_one = Number::One as i8;
+    let red_color: i32 = Color::Red as i32;
+
+    println!("{}", number_zero);
+    println!("{}", number_one);
+    println!("{}", red_color);
 }
