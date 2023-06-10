9818e5d5bd870e1cdea55da0fadaa696baadf0c2
Faical OUDBIB <faical.oudbib@ext.euronews.com>
2023-06-10 14:36:30 +0200

    from & into traits

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 // Code some rust and execute 'cargo run'
-fn main() {
+#[derive(Debug)]
+struct Number {
+    value: i32
+}
 
+impl From<i32> for Number {
+    fn from(v: i32) -> Self {
+        return Number { value: v };
+    }
+}
+
+fn main() {
+    let number = Number::from(7i32);
+    let another_number: Number = 17i32.into();
+
+    println!("Number 7 comes from {:?}", number);
+    println!("Number 7 comes from {:?}", another_number);
 }
