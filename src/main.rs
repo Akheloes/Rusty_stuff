dd9f62fc0da14c19af4980687f58e8f9a61d8912
Faical OUDBIB <faical.oudbib@ext.euronews.com>
2023-06-10 16:09:00 +0200

    convert to a string

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-// Code some rust and execute 'cargo run'
-fn main() {
+use std::fmt::Display;
 
+
+struct Circle<f32> {
+    radius: f32
+}
+
+impl Display for Circle<f32> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        
+        return write!(f, "Cercle de rayon {:?}", self.radius);
+    }
+}
+
+fn main() {
+    let circle = Circle { radius : 7.77 };
+
+    println!("{:?}", circle.to_string());
 }
